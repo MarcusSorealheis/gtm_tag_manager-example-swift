da58c8112720049e782e84848109e80eb8233e96
Marcus Eagan <marcuseagan@gmail.com>
2017-01-22 21:30:03 -0500

    passing various data across controllers.

--- WeDeliver/ProductsTableViewController.swift
+++ WeDeliver/ProductsTableViewController.swift
@@ -9,16 +9,34 @@
 import UIKit
 
 class ProductsTableViewController: UITableViewController {
+    
+    var productNames: [String]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        productNames = ["1907 Wall Set", "1921 Dial Phone",
+                        "1937 Desk Set", "1984 Motorola Portable"]
+    }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return 5
+        if let pNames = productNames {
+            return pNames.count
+        }
+        return 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->  UITableViewCell
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
         
-        cell.textLabel?.text = "Hello friend."
+        let productName = productNames?[indexPath.row]
+        
+        if let pName = productName {
+            
+            cell.textLabel?.text = pName
+        }
+
         cell.imageView?.image = UIImage(named: "image-cell1")
         
         return cell
@@ -26,7 +44,13 @@ class ProductsTableViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowProduct" {
             let productVC = segue.destination as? ProductViewController
-            productVC?.productName = "Really old phone"
+            
+            guard let cell = sender as? UITableViewCell,
+                let indexPath = tableView.indexPath(for: cell) else {
+                    return
+            }
+            productVC?.productName = productNames?[indexPath.row]
+            
         }
     }
 }
